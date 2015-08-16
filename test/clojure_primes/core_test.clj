31301930acd3ec8cf5ed74861f06591c5782a683
Jack Firth <jackhfirth@gmail.com>
2015-08-16 15:29:30 -0700

    Add prime generation functions
    Currently inefficient, as they do not properly exclude testing divisors
    above the square root of a number. However, as primes become less
    common above numbers (n / log n frequency), this is still better than
    linear time per prime for getting primes below n.

--- test/clojure_primes/core_test.clj
+++ test/clojure_primes/core_test.clj
@@ -9,3 +9,40 @@
   (testing "divisible-by-any? test"
     (is (divisible-by-any? 20 '(3 4 5)))
     (is (not (divisible-by-any? 17 '(3 4 5))))))
+
+
+(deftest ints-from-test
+  (testing "ints-from should return a lazy sequence of numbers starting at a number"
+    (is (= (take 10 (ints-from 5))
+           (list 5 6 7 8 9 10 11 12 13 14)))))
+
+
+(deftest find-first-test
+  (testing "find-first should return the first item in a sequence satisfying a predicate"
+    (is (= (find-first number? '(a b 2 c 3 5)) 2))))
+
+
+(deftest next-number-not-divisible-by-any-test
+  (testing (str "next-number-not-divisible-by-any should return the smallest number "
+                "greater than the lower bound that's coprime to divisors")
+    (is (= (next-number-not-divisible-by-any 7 '(2 3 5 7)) 11))))
+
+
+(deftest next-prime-test
+  (testing "next-prime should return the next number coprime to a given list of numbers"
+    (is (= (next-prime '(7 5 3 2)) 11))))
+
+
+(deftest next-prime-list-test
+  (testing "next-prime-list should add the next prime to a list of primes"
+    (is (= (next-prime-list '(7 5 3 2)) '(11 7 5 3 2)))))
+
+
+(deftest next-n-prime-list-test
+  (testing "next-n-prime-list should add the next n primes to a list of primes"
+    (is (= (next-n-prime-list 5 '(7 5 3 2)) '(23 19 17 13 11 7 5 3 2)))))
+
+
+(deftest first-n-primes-test
+  (testing "first-n-primes should return the first n primes"
+    (is (= (first-n-primes 9) '(23 19 17 13 11 7 5 3 2)))))
